809396d0f5180f0a2fcbb5fa32388a5ac51600f3
miteshprajapati <mitesh.prajapati@techivies.com>
2025-03-07 18:36:41 +0530

    improve login feature

--- frontend/src/component/Login.jsx
+++ frontend/src/component/Login.jsx
@@ -1,10 +1,27 @@
 import { useState } from "react";
+import { toast } from 'react-toastify';
 import '../style/Login.css';
 
 export default function Login() {
   const [loginForm, setLoginForm] = useState({ email: "", password: ""}); 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isLoading, setIsLoading] = useState(false); 
+  const [isShowPass, setIsShowPass] = useState(false);
+  const [error , setError] = useState({});
+
+
+  const validate = (name = null, value = null) => {
+    let errorMsg = "";
+    if (!value) {
+      errorMsg = `${name.charAt(0).toUpperCase() + name.slice(1)} is required.`;
+    } else if (name === "password" && value.length < 6) {
+      errorMsg = "Password must be at least 6 characters long.";
+    }
+    setError((prevErrors) => ({
+      ...prevErrors,
+      [name]: errorMsg,
+    }));
+  };
 
   async function handleLogin(event) {
     event.preventDefault();
@@ -19,18 +36,17 @@ export default function Login() {
       });
 
       const response = await user.json();
-      if (response) {
+      if (response.success) {
         setIsLoggedIn(true);
+        toast.success('Logged in successfully.')
       } else {
-        alert("Login failed!");
+        toast.error('Wrong Email or Password.');
       }
     } catch (error) {
-      console.error("Login error:", error);
-      alert("An error occurred while logging in.");
+      toast.error("An error occurred while logging in.");
     } finally {
       setIsLoading(false);
     }
-    console.log('loginform : ', loginForm);
   }
 
   function inputChangeHandler(event) {
@@ -39,6 +55,7 @@ export default function Login() {
       ...prevState,
       [name]: value,
     }));
+    validate(name, value);
   }
 
   return (
@@ -69,6 +86,7 @@ export default function Login() {
                 onChange={inputChangeHandler}
                 placeholder="Enter email"
               />
+              {error.email && <span className="error-message">{error.email}</span>}
             </div>
 
             <div className="form-group">
@@ -77,23 +95,33 @@ export default function Login() {
                 <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd"></path>
               </svg>
               <input
-                type="password"
+                type={isShowPass ? "text" : "password"}
                 name="password"
                 value={loginForm.password}
                 onChange={inputChangeHandler}
                 placeholder="Enter password"
               />
 
-              {/* need to implement */}
-              {/* <svg className="password-on" id="show-password" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                  <path d="M10 12a2 2 0 100-4 2 2 0 000 4z"></path>
-                  <path fillRule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clipRule="evenodd"></path>
-              </svg>
+              <div className="password-icon-container" onClick={() => setIsShowPass(!isShowPass)}>
 
-              <svg className="password-on" id="hide-password" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                <path fillRule="evenodd" d="M3.707 2.293a1 1 0 00-1.414 1.414l14 14a1 1 0 001.414-1.414l-1.473-1.473A10.014 10.014 0 0019.542 10C18.268 5.943 14.478 3 10 3a9.958 9.958 0 00-4.512 1.074l-1.78-1.781zm4.261 4.26l1.514 1.515a2.003 2.003 0 012.45 2.45l1.514 1.514a4 4 0 00-5.478-5.478z" clipRule="evenodd"></path>
-                <path d="M12.454 16.697L9.75 13.992a4 4 0 01-3.742-3.741L2.335 6.578A9.98 9.98 0 00.458 10c1.274 4.057 5.065 7 9.542 7 .847 0 1.669-.105 2.454-.303z"></path>
-              </svg> */}
+                {
+                  isShowPass ?
+                    ( 
+                      <svg id="show-password" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                        <path d="M10 12a2 2 0 100-4 2 2 0 000 4z"></path>
+                        <path fillRule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clipRule="evenodd"></path>
+                      </svg>
+                    )
+                    :
+                    ( 
+                      <svg id="hide-password" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                        <path fillRule="evenodd" d="M3.707 2.293a1 1 0 00-1.414 1.414l14 14a1 1 0 001.414-1.414l-1.473-1.473A10.014 10.014 0 0019.542 10C18.268 5.943 14.478 3 10 3a9.958 9.958 0 00-4.512 1.074l-1.78-1.781zm4.261 4.26l1.514 1.515a2.003 2.003 0 012.45 2.45l1.514 1.514a4 4 0 00-5.478-5.478z" clipRule="evenodd"></path>
+                        <path d="M12.454 16.697L9.75 13.992a4 4 0 01-3.742-3.741L2.335 6.578A9.98 9.98 0 00.458 10c1.274 4.057 5.065 7 9.542 7 .847 0 1.669-.105 2.454-.303z"></path>
+                      </svg>
+                    )
+                }
+              </div>
+              {error.password && <span className="error-message">{error.password}</span>}
 
             </div>
 
@@ -101,7 +129,7 @@ export default function Login() {
               <p>Forgot Password?</p>
             </div>
             <div className="login-btn-container">
-              <button className="login-btn" type="submit" disabled={isLoading}>
+              <button className="login-btn" type="submit" disabled={(!loginForm.email || !loginForm.password || loginForm.password.length < 6) ? true : false}>
                 {isLoading ? "Logging In..." : "Login"}
               </button>
             </div>
